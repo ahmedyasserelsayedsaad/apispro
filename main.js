d7c63266f0eeabfeb4d348fb736c97e6bc4ee90a
ahmedyasserelsayedsaad <145242396+ahmedyasserelsayedsaad@users.noreply.github.com>
2024-03-14 04:21:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,121 +1,3 @@
-/*
-const express = require('express');
-const books = require('./data')
-const app = express();
-const port = 3000;
-
-//to handle body
-app.use(express.json());
-
-//get all-books
-app.get('/books', (req, res) => {
-
-    res.json(books)
-});
-
-
-//get one book
-
-app.get('/books/:id', (req, res) => {
-    const bookId = Number(req.params.id)
-    console.log(bookId);
-    const book = books.find((one) => {
-
-        return one.id === bookId
-    });
-    if (!book) {
-        return res.status(400).json('not added yet , sorry...');
-    }
-    res.status(200).json(book)
-})
-
-//add new book
-
-app.post('/books', (req, res) => {
-    console.log(req.body);
-    const newBook = {
-        id: books.length + 1,
-        name: req.body.name,
-        price: req.body.price,
-    };
-    books.push(newBook);
-    res.status(200).json(books)
-})
-
-//update book
-
-app.patch('/books/:id', (res, req) => {
-    const bookId = +req.params.id;
-    let book=books.find((one)=>one.id===bookId);
-    if (!book) {
-        return res.status(404).json('not founded book , sorry...');
-    }
-    book={...book,...req.body}
-    console.log(book);
-    res.status(200).json(book)
-})
-
-//delete book
-
-app.delete('/books/:id',(req,res)=>{
-    const bookId= +req.params.id;
-    let book=books.filter((one)=>{
-        return one.id!==bookId;
-    });
-    console.log(book);
-    console.log(books);
-    res.status(200).json(book)
-})
-
-// server running
-app.listen(port, () => {
-    console.log(`server is running on port ${port}`)
-})*/
-/*
-const express=require('express');
-const app=express();
-const port=5000;
- app.use(express.json());
-app.get('/',(req,res)=>{
-    res.json('hello')
-});
-app.get('/weather/:city',(req,res)=>{
-    const city=req.params.city;
-})
-
-const express=require('express');
-const app = express();
-const apiKey = '40d5e99f83089298ed959d9af26769f2'; // ادخل API Keyكنت اخذتها من OpenWeatherMap ở الخطوة 1
-const port=5000;
-app.get('/weather', (req, res) => {
-    const city = req.query.city;
-    if (!city) {
-      return res.status(400).send('Please provide a city name as query parameter');
-    }
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  
-    const request = require('request');
-  
-    request(url, { json: true }, (err, response, body) => {
-      if (err) {
-        return res.status(500).send('Error fetching weather data');
-      }
-      if (response.statusCode === 200) {
-        res.send(body);
-      } else {
-        res.status(response.statusCode).send(body.message);
-      }
-    });
-  });
-
-
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`);
-});
-*/
-
-
-
 
 const express = require('express')
 const axios = require('axios');
@@ -172,13 +54,13 @@ app.get('/adhanTime/:city', async (req, res) => {
             let AData = await axios.get(api);
             let Ddata = await AData.data.coord;
             //  console.log(response.data);
-            const cname = new adhan.Coordinates(Ddata.lat, Ddata.lon); //بتحول الاحداثيات لموقع جغرافي
+            const cname = new adhan.Coordinates(Ddata.lat, Ddata.lon); 
             // console.log(cname);
             const date = new Date();
-            const para = adhan.CalculationMethod.Other(); // حساب اوقات الصلاة بناءا علي منظمة ما
+            const para = adhan.CalculationMethod.Other(); 
             const adhanT = new adhan.PrayerTimes(cname, date, para);
             const times = {
-                fajr: adhanT.fajr.toLocaleTimeString(),  // عشان احول الوقت الي حروف 
+                fajr: adhanT.fajr.toLocaleTimeString(),  
                 sunrise: adhanT.sunrise.toLocaleTimeString(),
                 dhuhr: adhanT.dhuhr.toLocaleTimeString(),
                 asr: adhanT.asr.toLocaleTimeString(),
@@ -307,70 +189,4 @@ app.listen(port, () => {
 
 
 
-/*
 
-
-
-
-  try {
-    // جلب الموقع الجغرافي للمدينة المُدخلة
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=YOUR_API_KEY`);
-    const location = response.data.coord;
-    const coordinates = new adhan.Coordinates(location.lat, location.lon);
-
-    // تعريف التاريخ الحالي
-    const date = new Date();
-
-    // تعريف المعاملات اللازمة لاسترجاع أوقات الصلاة
-    const params = adhan.CalculationMethod.Other(); // يمكنك استخدام طريقة حساب أخرى إذا رغبت
-
-    // استرجاع أوقات الصلاة
-    const prayerTimes = new adhan.PrayerTimes(coordinates, date, params);
-
-    // إرجاع أوقات الصلاة كاستجابة API
-    res.json({
-      fajr: prayerTimes.fajr.toLocaleTimeString(),
-      sunrise: prayerTimes.sunrise.toLocaleTimeString(),
-      dhuhr: prayerTimes.dhuhr.toLocaleTimeString(),
-      asr: prayerTimes.asr.toLocaleTimeString(),
-      maghrib: prayerTimes.maghrib.toLocaleTimeString(),
-      isha: prayerTimes.isha.toLocaleTimeString()
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-}); */
-
-/*
-app.get('/prayer-times', (req, res) => {
-  const { city } = req.query;
-
-  // تعريف الموقع الجغرافي للمدينة المُدخلة
-  const coordinates = new adhan.Coordinates(37.7749, -122.4194); // مثال: سان فرانسيسكو
-
-  // تعريف التاريخ الحالي
-  const date = new Date();
-
-  // تعريف المعاملات اللازمة لاسترجاع أوقات الصلاة
-  const params = adhan.CalculationMethod.Other(); // يمكنك استخدام طريقة حساب أخرى إذا رغبت
-
-  // استرجاع أوقات الصلاة
-  const prayerTimes = new adhan.PrayerTimes(coordinates, date, params);
-
-  // إرجاع أوقات الصلاة كاستجابة API
-  res.json({
-    fajr: prayerTimes.fajr.toLocaleTimeString(),
-    sunrise: prayerTimes.sunrise.toLocaleTimeString(),
-    dhuhr: prayerTimes.dhuhr.toLocaleTimeString(),
-    asr: prayerTimes.asr.toLocaleTimeString(),
-    maghrib: prayerTimes.maghrib.toLocaleTimeString(),
-    isha: prayerTimes.isha.toLocaleTimeString()
-  });
-});
-
-*/
